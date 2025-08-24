98af4bcc14d4cdfc1623fa2c18857988417c28f3
فارِع <faraa717281413@gmail.com>
2025-08-24 05:11:38 +0300

    Update More Fixex

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:vehicle_counter/app.dart';
+import 'package:vehicle_counter/core/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
